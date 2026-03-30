f33a518a945c56b785b338c22c20938cb10fe52b
Kavuma Hakim <hakimkavuma99@gmail.com>
2026-03-30 07:42:45 +0300

    Refactor Tailwind CSS content paths for improved specificity

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ import lineClamp from '@tailwindcss/line-clamp';
 export default {
   content: [
     './index.html',
-    './**/*.{js,ts,jsx,tsx}',
+    './App.{js,ts,jsx,tsx}',
+    './index.{js,ts,jsx,tsx}',
+    './DataContext.{js,ts,jsx,tsx}',
+    './constants.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './services/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class',
   theme: {
